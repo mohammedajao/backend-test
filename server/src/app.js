75dd8a1a35b2807856f8b547e7d971abc59d575a
WarhoIm <adrianwarholm@gmail.com>
2017-11-14 21:58:15 -0500

    Set up registration page

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,16 @@
-console.log('hello')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('combine'))
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hello world'
+    message: `Hello ${req.body.email}! Yours user was registered! Have fun.`
   })
 })
 
