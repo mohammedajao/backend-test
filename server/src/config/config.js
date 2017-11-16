783beb387a0b62d926462beafc8bfba18e413c8e
WarhoIm <adrianwarholm@gmail.com>
2017-11-16 06:55:23 -0500

    Updated server

---
+++ server/src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_Name || 'users',
+    user: process.env.DB_User || 'users',
+    password: process.env.DB_PASS || 'users',
+    options: {
+      dialect: process.env.DIALECT || 'sqlite',
+      host: process.env.HOST || 'localhost',
+      storage: './users.sqlite'
+    }
+  }
+}
